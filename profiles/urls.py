6f6cd873b130801b5e3226f2bdf18d165bc230e2
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-09-28 23:36:57 +0000

    Wire up profile url

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
+    path('add_post/', views.add_post, name='add_post'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('edit_post/<slug:slug>/', views.edit_post, name='edit_post'),
+    path('delete_post/<slug:slug>/', views.delete_post, name='delete'),
 ]
