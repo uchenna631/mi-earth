12f89a3c315530662526c809f18211b44ed2134d
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-09-28 07:47:42 +0000

    Modify user profile model

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import UserProfile
+from blog.models import Post
 from .forms import EditProfileForm
 
 
 def profile(request):
-    form = EditProfileForm()
+    """
+    Create profile view
+    """
+    user = request.user
+    posts = Post.objects.filter(author=user)
+    user_profile = UserProfile.objects.get(user=user)
+    form = EditProfileForm(
+        request.POST or None,
+        request.FILES or None,
+        instance=user_profile
+    )
+    if form.is_valid():
+        form.save()
     context = {
         'form': form,
+        'posts': posts
 
     }
     return render(request, 'profile.html', context)
