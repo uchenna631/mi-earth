e7ae1a8137933f5f5ca0ee58098d41b468b53a4f
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-09-29 00:39:14 +0000

    Deployment app

--- miplanetearth/settings.py
+++ miplanetearth/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["miplanetearth.herokuapp.com", "localhost"]
 
@@ -196,4 +198,3 @@ SUMMERNOTE_CONFIG = {
     ],
 }
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
